f2a3f5ed61c7674c8ff000a1297c28f63251ca89
Anna Malik <malik.anna1992@gmail.com>
2022-05-09 09:32:22 +0200

    section__container removed

--- src/Section/index.js
+++ src/Section/index.js
@@ -3,13 +3,11 @@ import "./style.css";
 
 const Section = ({ title, body, extraHeaderContent }) => (
     <section className="section">
-        <div className="section__container">
             <header className="section__header">
                 <h2>{title}</h2>
                 {extraHeaderContent}
             </header>
             {body}
-        </div>
     </section>
 )
 
